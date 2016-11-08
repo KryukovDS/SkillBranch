bb67e2f3542affcbc754f38a67cba2bb96354af8
Крюков Дмитрий <d.krukov@maximaster.ru>
2016-11-09 02:04:45 +0300

    Доработалзадание 2Б, добавил компонент для обработки кросс ориджин запросов

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 var express = require('express');
-var app = express();
-var port = 80;
+var cors = require('cors');
 
-app.get('/', function(req, res) {
+var app = express();
+var port = 3000;
+
+app.use(cors());
+app.listen(port, function () {
+    console.log('Слушаю порт ' + port + ', хозяин!');
+});
+
+app.all('/2b/', function(req, res) {
     var result = [];
+    var initials = [];
     var fullname = req.query.hasOwnProperty('fullname') ? req.query['fullname'] : '';
-    var initials = fullname.split(' ');
-    
-    if (initials.length && initials.length <= 3) {
-        initials.length && result.push(initials.pop());
-        initials.length && result.push(initials.shift().substr(0, 1) + '.');
-        initials.length && result.push(initials.pop().substr(0, 1) + '.');
+
+    if (!/[\d\t\n\r_=\-\+\*\\\/|]/.test(fullname)) {
+        initials = fullname.split(' ');
+        initials = initials.filter(function (val) {
+            return !!val.length;
+        });
+
+        initials = initials.map(function (val) {
+            return val.substr(0, 1).toUpperCase() + val.substr(1).toLocaleLowerCase();
+        });
+
+        if (initials.length && initials.length <= 3) {
+            initials.length && result.push(initials.pop());
+            initials.length && result.push(initials.shift().substr(0, 1) + '.');
+            initials.length && result.push(initials.pop().substr(0, 1) + '.');
+        }
     }
     
     result.length === 0 && result.push('Invalid fullname');
@@ -19,6 +37,3 @@ app.get('/', function(req, res) {
     res.send(result.join(' '));
 });
 
-app.listen(port, function () {
-    console.log('Слушаю порт ' + port + ', хозяин!');
-});
