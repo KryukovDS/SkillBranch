7d5b7b21b29976cce3b4f820dc6c5cde956ee360
Крюков Дмитрий <d.krukov@maximaster.ru>
2016-11-09 02:16:26 +0300

    Добавил первое задание рядом со вторым, после нужно перенести в node модули.

--- index.js
+++ index.js
@@ -2,14 +2,25 @@ var express = require('express');
 var cors = require('cors');
 
 var app = express();
-var port = 3000;
+var port = 80;
 
 app.use(cors());
 app.listen(port, function () {
     console.log('Слушаю порт ' + port + ', хозяин!');
 });
 
-app.all('/2b/', function(req, res) {
+// Решение первого задания второго курса
+app.get('/2a/', function(req, res) {
+    var a = req.query.hasOwnProperty('a') ? parseFloat(req.query['a']) || 0 : 0;
+    var b = req.query.hasOwnProperty('b') ? parseFloat(req.query['b']) || 0 : 0;
+    var result = a + b;
+
+    console.log('Ваши рабы посчитали сумму чисел a(' + a + ') и b(' + b + '), она составляет: ' + result + ', хозяин!');
+    res.status(200).send(result);
+});
+
+// Решение второго задания второго курса
+app.get('/2b/', function(req, res) {
     var result = [];
     var initials = [];
     var fullname = req.query.hasOwnProperty('fullname') ? req.query['fullname'] : '';
@@ -34,6 +45,5 @@ app.all('/2b/', function(req, res) {
     result.length === 0 && result.push('Invalid fullname');
 
     console.log('Full name: "' + fullname + '", result: "' + result.join(' ') + '".');
-    res.send(result.join(' '));
+    res.status(200).send(result.join(' '));
 });
-
